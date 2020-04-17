ece5e0041aa40021228407d63d7ac402594a5368
Elliot Blanchard <public@elliotblanchard.com>
2020-04-17 00:24:40 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,13 @@
-# code here!
+class School
+  #attr_accessor :color, :size, :material, :condition
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+end
